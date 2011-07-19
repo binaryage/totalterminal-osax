f5d0ea9a80e5082786bb235384350289a74e5cf7
Antonin Hildebrand <antonin@hildebrand.cz>
2011-07-20 01:46:45 +0200

    TotalFinder -> TotalTerminal

--- TotalTerminalInjector.m
+++ TotalTerminalInjector.m
@@ -3,13 +3,13 @@
 #import "TFStandardVersionComparator.h"
 #import "ini.h"
 
-#define TOTALFINDER_STANDARD_INSTALL_LOCATION "/Applications/TotalFinder.app"
-#define TOTALFINDER_INI_FILE "~/.totalfinder"
-#define FINDER_MIN_TESTED_VERSION @"10.6"
-#define FINDER_MAX_TESTED_VERSION @"10.6.8"
+#define TOTALTERMINAL_STANDARD_INSTALL_LOCATION "/Applications/TotalTerminal.app"
+#define TOTALTERMINAL_INI_FILE "~/.totalterminal"
+#define TERMINAL_MIN_TESTED_VERSION @"0"
+#define TERMINAL_MAX_TESTED_VERSION @"294"
 
 // SIMBL-compatible interface
-@interface TotalFinderPlugin: NSObject { 
+@interface TotalTerminalPlugin: NSObject { 
 }
 - (void) install;
 @end
@@ -46,42 +46,42 @@ static int ini_handler(void* user, const char* section, const char* name, const 
 }
 
 static void reportError(AppleEvent *reply, NSString* msg) {
-    NSLog(@"TotalFinderInjector: %@", msg);
+    NSLog(@"TotalTerminalInjector: %@", msg);
     AEPutParamString(reply, keyErrorString, msg);
 }
 
 OSErr HandleInitEvent(const AppleEvent *ev, AppleEvent *reply, long refcon) {
-    NSLog(@"TotalFinderInjector: Received init event");
+    NSLog(@"TotalTerminalInjector: Received init event");
     if (alreadyLoaded) {
-        NSLog(@"TotalFinderInjector: TotalFinder has been already loaded. Ignoring this request.");
+        NSLog(@"TotalTerminalInjector: TotalTerminal has been already loaded. Ignoring this request.");
         return noErr;
     }
     @try {
-        NSBundle* finderBundle = [NSBundle mainBundle];
-        if (!finderBundle) {
+        NSBundle* terminalBundle = [NSBundle mainBundle];
+        if (!terminalBundle) {
             reportError(reply, [NSString stringWithFormat:@"Unable to locate main Finder bundle!"]);
             return 4;
         }
         
-        NSString* finderVersion = [finderBundle objectForInfoDictionaryKey:@"CFBundleVersion"];
-        if (!finderVersion) {
+        NSString* terminalVersion = [terminalBundle objectForInfoDictionaryKey:@"CFBundleVersion"];
+        if (!terminalVersion) {
             reportError(reply, [NSString stringWithFormat:@"Unable to determine Finder version!"]);
             return 5;
         }
         
         // future compatibility check
-        NSString* supressKey = @"TotalFinderSuppressFinderVersionCheck";
+        NSString* supressKey = @"TotalTerminalSuppressFinderVersionCheck";
         NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
         if (![defaults boolForKey:supressKey]) {
             TFStandardVersionComparator* comparator = [TFStandardVersionComparator defaultComparator];
-            if (([comparator compareVersion:finderVersion toVersion:FINDER_MAX_TESTED_VERSION]==NSOrderedDescending) || 
-                ([comparator compareVersion:finderVersion toVersion:FINDER_MIN_TESTED_VERSION]==NSOrderedAscending)) {
+            if (([comparator compareVersion:terminalVersion toVersion:TERMINAL_MAX_TESTED_VERSION]==NSOrderedDescending) || 
+                ([comparator compareVersion:terminalVersion toVersion:TERMINAL_MIN_TESTED_VERSION]==NSOrderedAscending)) {
 
                 NSAlert* alert = [NSAlert new];
-                [alert setMessageText: [NSString stringWithFormat:@"You have Finder version %@", finderVersion]];
-                [alert setInformativeText: [NSString stringWithFormat:@"But TotalFinder was properly tested only with Finder versions in range %@ - %@\n\nYou have probably updated your system and Finder version got bumped by Apple developers.\n\nYou may expect a new TotalFinder release soon.", FINDER_MIN_TESTED_VERSION, FINDER_MAX_TESTED_VERSION]];
+                [alert setMessageText: [NSString stringWithFormat:@"You have Finder version %@", terminalVersion]];
+                [alert setInformativeText: [NSString stringWithFormat:@"But TotalTerminal was properly tested only with Finder versions in range %@ - %@\n\nYou have probably updated your system and Finder version got bumped by Apple developers.\n\nYou may expect a new TotalTerminal release soon.", TERMINAL_MIN_TESTED_VERSION, TERMINAL_MAX_TESTED_VERSION]];
                 [alert setShowsSuppressionButton:YES];
-                [alert addButtonWithTitle:@"Launch TotalFinder anyway"];
+                [alert addButtonWithTitle:@"Launch TotalTerminal anyway"];
                 [alert addButtonWithTitle:@"Cancel"];
                 NSInteger res = [alert runModal];
                 if ([[alert suppressionButton] state] == NSOnState) {
@@ -95,14 +95,14 @@ OSErr HandleInitEvent(const AppleEvent *ev, AppleEvent *reply, long refcon) {
         
         // read install location from ini file if present, otherwise use standard install location
         configuration config;
-        NSString* totalFinderLocation = @TOTALFINDER_STANDARD_INSTALL_LOCATION;
-        NSString* iniPath = [@TOTALFINDER_INI_FILE stringByExpandingTildeInPath];
+        NSString* totalFinderLocation = @TOTALTERMINAL_STANDARD_INSTALL_LOCATION;
+        NSString* iniPath = [@TOTALTERMINAL_INI_FILE stringByExpandingTildeInPath];
         if (ini_parse([iniPath cStringUsingEncoding:NSASCIIStringEncoding], ini_handler, &config) >= 0) {
             totalFinderLocation = [config.location stringByStandardizingPath];
             [config.location release];
         }
         
-        NSBundle* pluginBundle = [NSBundle bundleWithPath:[totalFinderLocation stringByAppendingPathComponent:@"Contents/Resources/TotalFinder.bundle"]];
+        NSBundle* pluginBundle = [NSBundle bundleWithPath:[totalFinderLocation stringByAppendingPathComponent:@"Contents/Resources/TotalTerminal.bundle"]];
         if (!pluginBundle) {
             reportError(reply, [NSString stringWithFormat:@"Unable to create bundle from path: %@", totalFinderLocation]);
             return 2;
@@ -114,19 +114,19 @@ OSErr HandleInitEvent(const AppleEvent *ev, AppleEvent *reply, long refcon) {
             return 6;
         }
         
-        TotalFinderPlugin* principalClass = (TotalFinderPlugin*)[pluginBundle principalClass];
+        TotalTerminalPlugin* principalClass = (TotalTerminalPlugin*)[pluginBundle principalClass];
         if (!principalClass) {
             reportError(reply, [NSString stringWithFormat:@"Unable to retrieve principalClass for bundle: %@", pluginBundle]);
             return 3;
         }
         if ([principalClass respondsToSelector:@selector(install)]) {
-            NSLog(@"TotalFinderInjector: Installing TotalFinder ...");
+            NSLog(@"TotalTerminalInjector: Installing TotalTerminal ...");
             [principalClass install];
         }
         alreadyLoaded = true;
         return noErr;
     } @catch (NSException* exception) {
-        reportError(reply, [NSString stringWithFormat:@"Failed to load TotalFinder with exception: %@", exception]);
+        reportError(reply, [NSString stringWithFormat:@"Failed to load TotalTerminal with exception: %@", exception]);
     }
     return 1;
 }
@@ -135,6 +135,6 @@ OSErr HandleCheckEvent(const AppleEvent *ev, AppleEvent *reply, long refcon) {
     if (alreadyLoaded) {
         return noErr;
     }
-    reportError(reply, @"TotalFinder not loaded");
+    reportError(reply, @"TotalTerminal not loaded");
     return 1;
 }
