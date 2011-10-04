a996c98afaeee4202ff360f70f13ece4bee61e67
Antonin Hildebrand <antonin@hildebrand.cz>
2011-10-04 20:12:06 +0400

    bump Terminal.app compatibility to 299

--- TotalTerminalInjector.m
+++ TotalTerminalInjector.m
@@ -4,7 +4,7 @@
 
 #define TOTALTERMINAL_STANDARD_INSTALL_LOCATION "/Applications/TotalTerminal.app"
 #define TERMINAL_MIN_TESTED_VERSION @"0"
-#define TERMINAL_MAX_TESTED_VERSION @"297"
+#define TERMINAL_MAX_TESTED_VERSION @"299"
 
 // SIMBL-compatible interface
 @interface TotalTerminalPlugin: NSObject { 
