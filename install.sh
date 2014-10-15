50fc8d9044c63e602e5f67cba6c948253051c5d9
Antonin Hildebrand <antonin@hildebrand.cz>
2014-10-15 14:31:29 +0100

    cleanup: ini subfolder is not used anymore

--- install.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-# you should run xcodebuild to compile bin/TotalTerminal.osax
-
-sudo rm -rf /Library/ScriptingAdditions/TotalTerminal.osax
-sudo cp -r bin/TotalTerminal.osax /Library/ScriptingAdditions
